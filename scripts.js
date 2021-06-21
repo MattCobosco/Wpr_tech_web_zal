f117cc0c6569aafca1ebdebf7b2e6cca51f089c8
MattCobosco <mateuszkalucki@gmail.com>
2021-06-21 23:20:54 +0200

    formatting, some scripts moved to js file

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleContactForm() {
+    var x = document.getElementById("contact_form_open");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } 
+    else {
+      x.style.display = "block";
+    }
+}
