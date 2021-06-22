8fe1f69503fe086d6159a196dd03870e0466c1d0
MattCobosco <mateuszkalucki@gmail.com>
2021-06-22 16:05:47 +0200

    finished v 1.0

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 function toggleContactForm() {
-    var x = document.getElementById("contact_form_open");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } 
-    else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("contact_form_open");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  }
+}
+
+function showNav() {
+  var x = document.getElementById("nav_main");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
